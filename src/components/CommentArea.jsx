0a5c9c0c003b2450026d0c165a9f6b13b1bd7d2e
Joselinedesouza <joselinedesouza022@gmail.com>
2025-04-08 15:11:40 +0200

    CommentArea-OK

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CommentList from "../components/CommentLists";
 import AddComment from "../components/AddComments";
 const URL = `https://striveschool-api.herokuapp.com/api/comments`;
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-  };
+const CommentArea = function (props) {
+  const [comments, setComments] = useState([]);
 
-  getComments = () => {
-    if (!this.props.asin) return; // <-- evita fetch inutili
-  
-    fetch(URL + "/" + this.props.asin, {
+  const getComments = () => {
+    if (!props.asin) return; // <-- evita fetch inutili
+
+    fetch(URL + "/" + props.asin, {
       headers: {
         authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2Y0ZWJhNzgxYjBkZDAwMTUwYTdhM2UiLCJpYXQiOjE3NDQxMDQzNTksImV4cCI6MTc0NTMxMzk1OX0.NHMyM-ReX8Ho5M5EHi7i6ItGTvUfxV6FlNuaX9cS3-c",
@@ -26,28 +24,26 @@ class CommentArea extends Component {
       })
       .then((data) => {
         console.log("DATA", data); // verifica che arrivi un array
-        this.setState({ comments: data });
+        setComments(data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
-  
 
-  componentDidUpdate = (prevProps) => {
-    if (prevProps.asin !== this.props.asin) {
-      console.log("ASIN aggiornato!", this.props.asin);
-      this.getComments();
-    }
-  };
-  render() {
-    return (
-      <div className="comment-area mt-3">
-        <AddComment asin={this.props.asin} />
-        <CommentList comments={this.state.comments} />
-      </div>
-    );
-  }
-}
+  useEffect(
+    function () {
+      getComments();
+    },
+    [props.asin]
+  );
+
+  return (
+    <div className="comment-area mt-3">
+      <AddComment asin={props.asin} />
+      <CommentList comments={comments} />
+    </div>
+  );
+};
 
 export default CommentArea;
