a9d0fd847c3a1d5dc29d4ac6dc0622f9a080ddf3
pandino126 <re.luca95@gmail.com>
2025-04-09 00:44:15 +0200

    Testo welcome ingrandito

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,20 +4,20 @@ function AlertWelcome({ className }) {
   return (
     <Alert variant="warning" className={`special-elite-regular text-center bg-black w-100 ${className}`}>
       <Alert.Heading className="fw-bold fs-4 text-white">
-        Welcome in Epibooks
+        WELCOME IN EPIBOOKS BY JOS E LUCA
       </Alert.Heading>
-      <p className="text-warning special-elite-regular">Troverai qui il libro dei tuoi incubi</p>
+      <p className="text-warning special-elite-regular fs-5">Troverai qui il libro dei tuoi incubi</p>
       <hr />
       <p className="mb-0">
-        <span className="text-info opacity-75 special-elite-regular">
+        <span className="text-info opacity-75 special-elite-regular fs-5">
           “Alcuni pensano che io sia una persona orribile, ma non è vero.
         </span>
         <br />
-        <span className="text-success opacity-75 special-elite-regular">
+        <span className="text-success opacity-75 special-elite-regular fs-5">
           Io ho il cuore di un bambino -
         </span>
         <br />
-        <span className="fw-bold text-danger special-elite-regular">
+        <span className="fw-bold text-danger special-elite-regular fs-5">
           in un barattolo sulla mia scrivania.” (Stephen King)
         </span>
       </p>
